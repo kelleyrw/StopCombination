82ca81262beef766de3981615e3a778ea3c0ad5f
kelleyrw <kelleyrw@gmail.com>
2014-06-01 21:16:06 -0700

    fixed setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-[ -z "$CMSSW_BASE" ] && echo "Need to set CMSSW_BASE" && exit 1;
+[ -z "$CMSSW_BASE" ] && echo "[setup.sh] need to set CMSSW_BASE" && exit 1;
 
 pushd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
 
-#Checkout the analysis tools:
+# Checkout the analysis tools:
+echo "[setup.sh] checking out AnalysisTools"
 git clone https://github.com/kelleyrw/AnalysisTools.git
 
-#Checkout the fork of Higg's Combine limit setting tool:
+# Checkout the fork of Higg's Combine limit setting tool:
+echo "\n[setup.sh] checking out Higg's Combine"
 git clone https://github.com/RazorCMS/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 pushd HiggsAnalysis/CombinedLimit/
 git checkout onelepcomb_032614
 popd
 
-#Checkout CMS2's CORE (TODO: refactor AnalysisTools so that this is not dependent)
+# Checkout CMS2's CORE (TODO: refactor AnalysisTools so that this is not dependent)
+echo "\n[setup.sh] checking out CMS2"
 git clone https://github.com/cmstas/Dictionaries.git CMS2/Dictionaries
 git clone https://github.com/cmstas/CORE.git CMS2/NtupleMacrosCore
 pushd CMS2/NtupleMacrosCore
@@ -24,4 +27,12 @@ git checkout scram_compliant
 popd
 
 # Compile:
+echo "\n[setup.sh] compiling"
 scram b -j20
+
+# done
+if [ $? -eq 0]; then
+	echo "\n[setup.sh] setup successful"
+else
+	echo "\n[setup.sh] setup failed"
+fi
