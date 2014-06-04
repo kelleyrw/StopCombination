4991a052b7acbddabba5aebde469e59adfb1c3cc
Ryan <rwkelley@physics.ucsd.edu>
2014-06-04 11:56:09 -0700

    fixing up batch

--- batch/job_input/copy_files.sh
+++ batch/job_input/copy_files.sh
@@ -4,10 +4,9 @@
 rm -r t2tt
 
 # copy relevant files
-cp -r $CMSSW_BASE/src/StopAnalysis/CombinedExclusions/scripts/stop_produce_limit.py . 
-# cp -r $CMSSW_BASE/src/StopAnalysis/CombinedExclusions/cards/t2tt/combined t2tt 
-cp -r $CMSSW_BASE/src/StopAnalysis/CombinedExclusions/cards/t2tt/combined_22May2014 t2tt 
-cp -r $CMSSW_BASE/src/StopAnalysis/CombinedExclusions/batch_combine/sweepRoot.cc .
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/scripts/stop_produce_limit.py . 
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/cards/t2tt/combined t2tt 
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/batch/sweepRoot.cc .
 
 # tar up inputs
 tar -czf input.tgz  --exclude='*.git*' *
--- batch/writeAllConfig.sh
+++ batch/writeAllConfig.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-OUTPUT_PATH=limits/lep1_razor_combine/hybrid_22may
-METHOD=3 # 1: asymptotic, 2: hybrid new, 3: hybrid new with pre-generated grid
-CARD_PATH=$CMSSW_BASE/src/StopAnalysis/CombinedExclusions/batch_full_combine/job_input
+CARD_PATH=$CMSSW_BASE/src/StopAnalysis/CombinedExclusions/batch/job_input
 
 # scans
-./writeConfig.sh t2tt $CARD_PATH/t2tt $OUTPUT_PATH/t2tt $METHOD
+OUTPUT_PATH=limits/stopcombo/v0/assymptotic
+METHOD=3 # 1: asymptotic, 2: hybrid new, 3: hybrid new with pre-generated grid
+./writeConfig.sh t2tt $CARD_PATH/t2tt $OUTPUT_PATH/t2tt $METHOD                
 
-OUTPUT_PATH=limits/lep1_razor_combine/asymptotic_22may
+OUTPUT_PATH=limits/stopcombo/v0/hybrid
 METHOD=1
-
 ./writeConfig.sh t2tt $CARD_PATH/t2tt $OUTPUT_PATH/t2tt $METHOD
