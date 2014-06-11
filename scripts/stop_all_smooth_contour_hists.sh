df3bb06a6bab162dc1d915b2f3c7463a05e3ec21
kelleyrw <kelleyrw@gmail.com>
2014-06-10 21:34:45 -0700

    updated polished plotting routine

--- scripts/stop_all_smooth_contour_hists.sh
+++ scripts/stop_all_smooth_contour_hists.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create the histograms and TGraphs
-# python scripts/stop_create_contour_hists.py --analysis razor    --method asymptotic
-# python scripts/stop_create_contour_hists.py --analysis onelep   --method asymptotic
-# python scripts/stop_create_contour_hists.py --analysis combined --method asymptotic
-# python scripts/stop_create_contour_hists.py --analysis razor    --method hybrid
-# python scripts/stop_create_contour_hists.py --analysis onelep   --method hybrid
-# python scripts/stop_create_contour_hists.py --analysis combined --method hybrid
+python scripts/stop_create_contour_hists.py --analysis razor    --method asymptotic
+python scripts/stop_create_contour_hists.py --analysis onelep   --method asymptotic
+python scripts/stop_create_contour_hists.py --analysis combined --method asymptotic
+python scripts/stop_create_contour_hists.py --analysis razor    --method hybrid
+python scripts/stop_create_contour_hists.py --analysis onelep   --method hybrid
+python scripts/stop_create_contour_hists.py --analysis combined --method hybrid
 
 # polished SMS plots
 python $CMSSW_BASE/src/AnalysisTools/PlotsSMS/python/makeSMSplots.py pset/T2tt_SUS13004_ASYMPTOTIC.cfg plots/limits/v0/asymptotic/t2tt/razor/T2tt_RAZOR_ASYMPTOTIC_ 
