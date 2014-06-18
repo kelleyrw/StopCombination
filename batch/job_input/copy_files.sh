4094248e1d5025d6ce2f9253131f902a2d4caad0
Ryan <rwkelley@physics.ucsd.edu>
2014-06-18 08:50:01 -0700

    updated batch for new samples

--- batch/job_input/copy_files.sh
+++ batch/job_input/copy_files.sh
@@ -2,10 +2,14 @@
 
 # remove old cards
 if [ -d t2tt ]; then rm -r t2tt; fi
+if [ -d t2bw ]; then rm -r t2bw; fi
+if [ -d t2tb_br0p5 ]; then rm -r t2tb_br0p5; fi
 
 # copy relevant files
 cp -r $CMSSW_BASE/src/Analysis/StopCombination/scripts/stop_produce_limit.py . 
-cp -r $CMSSW_BASE/src/Analysis/StopCombination/cards/t2tt/combined t2tt 
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/cards/t2tt/combined       t2tt
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/cards/t2bw/combined       t2bw
+cp -r $CMSSW_BASE/src/Analysis/StopCombination/cards/t2tb_br0p5/combined t2tb_br0p5 
 cp -r $CMSSW_BASE/src/Analysis/StopCombination/batch/sweepRoot.cc .
 
 # tar up inputs
--- batch/writeAllConfig.sh
+++ batch/writeAllConfig.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-CARD_PATH=$CMSSW_BASE/src/StopAnalysis/CombinedExclusions/batch/job_input
+CARD_PATH=$CMSSW_BASE/src/Analysis/StopCombination/batch/job_input
 
 # scans
 OUTPUT_PATH=limits/stopcombo/v0/hybrid
 METHOD=3 # 1: asymptotic, 2: hybrid new, 3: hybrid new with pre-generated grid
-./writeConfig.sh t2tt $CARD_PATH/t2tt $OUTPUT_PATH/t2tt $METHOD                
+./writeConfig.sh t2tt       $CARD_PATH/t2tt       $OUTPUT_PATH/t2tt       $METHOD                
+./writeConfig.sh t2tb_br0p5 $CARD_PATH/t2tb_br0p5 $OUTPUT_PATH/t2tb_br0p5 $METHOD
+./writeConfig.sh t2bw       $CARD_PATH/t2bw       $OUTPUT_PATH/t2bw       $METHOD
 
 OUTPUT_PATH=limits/stopcombo/v0/asymptotic
 METHOD=1
-./writeConfig.sh t2tt $CARD_PATH/t2tt $OUTPUT_PATH/t2tt $METHOD
+./writeConfig.sh t2tt       $CARD_PATH/t2tt       $OUTPUT_PATH/t2tt       $METHOD
+./writeConfig.sh t2tb_br0p5 $CARD_PATH/t2tb_br0p5 $OUTPUT_PATH/t2tb_br0p5 $METHOD
+./writeConfig.sh t2bw       $CARD_PATH/t2bw       $OUTPUT_PATH/t2bw       $METHOD
