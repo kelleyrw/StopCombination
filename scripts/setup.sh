034f85d5fe4e0f20c393da0259fa33a579dd374f
kelleyrw <kelleyrw@gmail.com>
2014-06-30 08:27:40 -0700

    updated README and setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,22 +8,15 @@ eval `scramv1 runtime -sh`
 # Checkout the analysis tools:
 echo -e "[setup.sh] checking out AnalysisTools"
 git clone https://github.com/kelleyrw/AnalysisTools.git
+pushd AnalysisTools
+git checkout stopcombo_V01-00-00
+popd
 
 # Checkout the fork of Higg's Combine limit setting tool:
 echo -e "\n[setup.sh] checking out Higg's Combine"
 git clone https://github.com/RazorCMS/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 pushd HiggsAnalysis/CombinedLimit/
 git checkout onelepcomb_032614
-popd
-
-# Checkout CMS2's CORE (TODO: refactor AnalysisTools so that this is not dependent)
-echo -e "\n[setup.sh] checking out CMS2"
-git clone https://github.com/cmstas/Dictionaries.git CMS2/Dictionaries
-git clone https://github.com/cmstas/CORE.git CMS2/NtupleMacrosCore
-pushd CMS2/NtupleMacrosCore
-git pull
-git checkout scram_compliant
-./setup/setupCoreForSCRAM.sh setup/cms2_ntuple_postprocess_05.03.23.root
 popd
 
 # Compile:
